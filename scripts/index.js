113bd3eecca0560b6730d33ac84eec2ff2ebf51f
Dmitriy Malyshev <dimmal-85@mail.ru>
2023-04-19 16:39:34 +0300

    Создание JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,26 @@
 const popupElement = document.querySelector('.popup');
-console.log(popupElement);
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupOpenButtonElement = document.querySelector('.profile__button-edit');
+const popupSafeButtonElement = popupElement.querySelector('.popup__safe');
+const togglePopupVisibility = function () {
+    popupElement.classList.toggle('popup_opened');
+};
+popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
+popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
+const galleryLikeElements = document.querySelectorAll('.gallery__like');
+galleryLikeElements.forEach(function (item, index) {
+    item.addEventListener('click', () => item.classList.toggle('gallery__like_active'))
+});
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__data-name');
+let jobInput = formElement.querySelector('.popup__data-profession');
+let profileName = document.querySelector('h1');
+let profileJob = document.querySelector('p');
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+};
+formElement.addEventListener('submit', handleFormSubmit);
+popupSafeButtonElement.addEventListener('click', handleFormSubmit);
+popupSafeButtonElement.addEventListener('click', togglePopupVisibility);
