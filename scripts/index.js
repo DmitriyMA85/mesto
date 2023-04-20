86ed2ee4e59ba38d74a0b10f6e960a488bba3a91
Dmitriy Malyshev <dimmal-85@mail.ru>
2023-04-21 00:38:15 +0300

    После первой проверки

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,28 @@
 const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-const popupOpenButtonElement = document.querySelector('.profile__button-edit');
-const popupSafeButtonElement = popupElement.querySelector('.popup__safe');
+const popupCloseButtonElement = popupElement.querySelector('.button-close');
+const popupOpenButtonElement = document.querySelector('.button-edit');
 const togglePopupVisibility = function () {
     popupElement.classList.toggle('popup_opened');
 };
 popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
 popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
-const galleryLikeElements = document.querySelectorAll('.gallery__like');
-galleryLikeElements.forEach(function (item, index) {
-    item.addEventListener('click', () => item.classList.toggle('gallery__like_active'))
-});
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__data-name');
-let jobInput = formElement.querySelector('.popup__data-profession');
-let profileName = document.querySelector('h1');
-let profileJob = document.querySelector('p');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_data_name');
+let jobInput = formElement.querySelector('.popup__input_data_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+const addinputdata = function () {
+    nameInput.value.textContent=profileName;
+}
+addinputdata ();
+console.log(nameInput);
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+    togglePopupVisibility();
 };
 formElement.addEventListener('submit', handleFormSubmit);
-popupSafeButtonElement.addEventListener('click', handleFormSubmit);
-popupSafeButtonElement.addEventListener('click', togglePopupVisibility);
 formElement.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
         togglePopupVisibility();
